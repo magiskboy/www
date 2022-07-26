991f67603c70aa30de93d535c11d507333058287
Nguyen Khac Thanh <thanh.nk@teko.vn>
2022-07-26 08:51:30 +0700

    update

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -2,14 +2,14 @@ import "https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js";
 import "https://www.gstatic.com/firebasejs/8.1.2/firebase-analytics.js";
 
 var firebaseConfig = {
-  apiKey: "{{ .Site.Params.Firebase.ApiKey }}",
-  authDomain: "{{ .Site.Params.Firebase.AuthDomain }}",
-  databaseURL: "{{ .Site.Params.Firebase.DatabaseURL }}",
-  projectId: "{{ .Site.Params.Firebase.ProjectId }}",
-  storageBucket: "{{ .Site.Params.Firebase.StorageBucket }}",
-  messagingSenderId: "{{ .Site.Params.Firebase.SenderId }}",
-  appId: "{{ .Site.Params.Firebase.AppId }}",
-  measurementId: "{{ .Site.Params.Firebase.MeasurementId }}"
+  apiKey: "AIzaSyAny8MQuh4uEziV2FsJAEBMHvRcCjHeDj0",
+  authDomain: "myblog-e552f.firebaseapp.com",
+  databaseURL: "https://myblog-e552f-default-rtdb.firebaseio.com",
+  projectId: "myblog-e552f",
+  storageBucket: "myblog-e552f.appspot.com",
+  messagingSenderId: "488762331302",
+  appId: "1:488762331302:web:8edebcb8c2116cff0b2467",
+  measurementId: "G-WVWZEJLY14"
 };
 
 firebase.initializeApp(firebaseConfig);
