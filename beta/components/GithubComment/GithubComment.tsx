5d5e82235019198aa02e9ea23e742f60b06eb73e
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2022-10-22 15:35:06 +0700

    update

--- beta/components/GithubComment/GithubComment.tsx
+++ beta/components/GithubComment/GithubComment.tsx
@@ -13,7 +13,6 @@ const GithubComment: React.FC = () => {
     commentScript.async = true;
     commentScript.src = 'https://utteranc.es/client.js';
     commentScript.setAttribute('repo', utterancRepo);
-    commentScript.setAttribute('issue-term', 'pathname');
     commentScript.setAttribute('id', 'utterances');
     commentScript.setAttribute('label', 'comment');
     commentScript.setAttribute('theme', commentsTheme);
