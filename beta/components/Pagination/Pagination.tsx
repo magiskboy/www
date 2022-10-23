1e51faad0845bda87b3574f9837fa24ecf3abc28
Nguyen Khac Thanh <thanh.nk@teko.vn>
2022-10-23 22:24:10 +0700

    chore: update

--- beta/components/Pagination/Pagination.tsx
+++ beta/components/Pagination/Pagination.tsx
@@ -7,13 +7,13 @@ export const Pagination: React.FC<PaginationProps> = ({ pagination }) => {
     <div className={style.root}>
       {pagination.hasPrevious && (
         <span>
-          <Link href={`/page/${pagination.current - 1}`}>« Trước</Link>—
+          <Link href={`/page/${pagination.current - 1}`}>« Trước</Link>
         </span>
       )}
       <span>{pagination.current}</span>
       {pagination.hasNext && (
         <span>
-          — <Link href={`/page/${pagination.current + 1}`}>« Tiếp</Link>
+          <Link href={`/page/${pagination.current + 1}`}>Tiếp »</Link>
         </span>
       )}
     </div>
