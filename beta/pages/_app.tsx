3ecb77a1794612c58dda8a9ed10073599001c5d3
Nguyen Khac Thanh <thanh.nk@teko.vn>
2022-10-26 08:52:21 +0700

    chore: update theme

--- beta/pages/_app.tsx
+++ beta/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.scss";
-import "../styles/github.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { Router } from "next/router";
