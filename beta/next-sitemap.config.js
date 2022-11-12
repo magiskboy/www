b3a2bd920892c3120e3ec335a6a7647ff41122b6
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2022-11-12 15:00:09 +0700

    chore: update domain  for sitemap

--- beta/next-sitemap.config.js
+++ beta/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://blog.nkthanh.dev",
+  siteUrl: process.env.SITE_URL || "https://nkthanh.dev",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
