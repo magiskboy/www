76288d945f6fba82d7434df2868063decf52062d
Nguyen Khac Thanh <nkthanh@Nguyens-MacBook-Pro.local>
2023-06-28 05:41:45 +0700

    well done

--- legacy-page-redirect.js
+++ legacy-page-redirect.js
@@ -3,6 +3,11 @@ const redirects = [
         source: "/posts/bat-dong-bo-trong-python-phan-1-coroutine",
         destination: "/posts/asynchronous-in-python-part-1",
         permanent: true,
+    },
+    {
+        source: "/posts/bat-dong-bo-trong-python-phan-2-coroutine",
+        destination: "/posts/asynchronous-in-python-part-2",
+        permanent: true,
     }
 ];
 
